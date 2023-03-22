613e1bca0bcece3aa02a7f25de018a87dd6fc9d6
Ismail <codeismail@gmail.com>
2023-03-22 15:59:52 +0100

    update statusbar style

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   
   return (<>
-  <StatusBar style='auto'/>
+  <StatusBar style='light'/>
   <NavigationContainer>
     <Stack.Navigator screenOptions={
       {
